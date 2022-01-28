c743d2066124c892051c61e00ffe6da6d7ef1c6b
Alexey Chernikov <millgalid@gmail.com>
2022-01-28 19:48:58 +0300

    Add UnsupportedMessageType;

---
+++ Telegram/LongBox.Telegram.Domain/Exceptions/UnsupportedMessageType.cs
@@ -0,0 +1,9 @@
+﻿using Telegram.Bot.Types.Enums;
+
+namespace LongBox.Telegram.Domain.Exceptions;
+
+public sealed class UnsupportedMessageType : Exception
+{
+	public UnsupportedMessageType(UpdateType updateType) : base($"Неподдерживаемый тип сообщения: {updateType}")
+	{ }
+}
